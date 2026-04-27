9a33a168029c69f2699201f909220d4667621454
Vadym <vglovny25@students.oamk.fi>
2026-04-28 00:38:51 +0300

    Refactor code structure for improved readability and maintainability

--- servers/helpers/db.js
+++ servers/helpers/db.js
@@ -1,31 +1,18 @@
 require('dotenv').config()
 const { Pool } = require('pg')
 
-const query = (sql,values = []) => {
-    return new Promise(async(resolve,reject) => {
-        try{
-            const pool = openDb()
-            const result = await pool.query(sql,values)
-            resolve(result)
-        } catch(error){
-            reject(error.message)
-        }
-    })
-}
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
 
-const openDb = () => {
-    const pool = new Pool({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    })
-    return pool
-}
+const query = (sql, values = []) => pool.query(sql, values)
 
 module.exports = {
     query
